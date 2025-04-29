3f49edc70c5f7db7c7b5cbf8852b0d47f23a549d
Mohamed Saad <sa3doni2714@gmail.com>
2025-04-29 15:37:43 +0300

    Last

--- resources/views/website/single-product.blade.php
+++ resources/views/website/single-product.blade.php
@@ -317,7 +317,7 @@
                         <div class="product-card">
                             <div class="product-image">
                                 <a href="{{ route('product.show', $relatedProduct->id) }}">
-                                    <img src="{{ $relatedProduct->productImages->first()->image_url ?? 'assets/images/placeholder.jpg' }}" alt="{{ $relatedProduct->{'name_' . app()->getLocale()} }}" class="img-fluid">
+                                    <img src="{{ asset($relatedProduct->productImages->first()->image_url ?? 'assets/images/placeholder.jpg') }}" alt="{{ $relatedProduct->{'name_' . app()->getLocale()} }}" class="img-fluid">
                                 </a>
                                 <div class="product-action">
                                     <a href="#" class="quick-view" data-product-id="{{ $relatedProduct->id }}"><i class="lni lni-eye"></i></a>
