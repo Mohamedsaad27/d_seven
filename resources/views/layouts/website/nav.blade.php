1d4dad5d02d6a7fab1a34a6f1d70eb5a026b56a5
Mohamed Saad <sa3doni2714@gmail.com>
2025-05-18 15:45:18 +0300

    fixing

--- resources/views/layouts/website/nav.blade.php
+++ resources/views/layouts/website/nav.blade.php
@@ -143,7 +143,7 @@
                                         <ul class="shopping-list">
                                             @auth
                                                 @php
-                                                $cart = \App\Models\Cart::with(['cartItems.product.productImages', 'cartItems.product.discounts', 'cartItems.color.color', 'cartItems.product.brand'])
+                                                $cart = \App\Models\Cart::with(['cartItems.product.productImages', 'cartItems.product.discounts', 'cartItems.product.brand'])
                                                         ->where('user_id', auth()->id())
                                                         ->orderBy('created_at', 'desc')
                                                         ->first();
